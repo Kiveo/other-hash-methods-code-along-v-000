a5e3b55e19da65fe8428a7f78e9230b79630f257
Maverick Tesini <mavericktesini@gmail.com>
2018-03-12 19:36:32 +0000

    automatically backed up by learn

--- lib/values.rb
+++ lib/values.rb
@@ -10,11 +10,8 @@ require 'pry'
 
 def get_the_values(groceries)
   #code your solution here!
-  array1 = []
-  groceries.each do |cat, subcat|
-    groceries.cat.each do |subcat, detail|
-      array1 << detail
-    end
+  groceries.values.each do |category, detail|
+    array1 << detail
   end
   array1
 end
